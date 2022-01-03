fabcf6102124f1933b5d75ab14a3736aa015237b
artem <artem.fediai@kit.edu>
2022-01-03 21:24:05 +0100

    reconstruction works

--- two.py
+++ two.py
@@ -48,6 +48,8 @@ plt.savefig(os.path.join(OUT_PATH, 'original_data.png'), dpi=600)
 
 # Compute covariance matrix
 XY = np.array([X, Y])
+#XYmean = [np.mean(X), np.mean(Y)]
+#print(f'mean {XYmean}')
 print('\nX, Y array:\n', np.array(XY))
 cov_mat = np.cov(XY)
 print('\ncovariance matrix: \n', cov_mat)
@@ -55,28 +57,35 @@ print('\ncovariance matrix: \n', cov_mat)
 # Compute PCs as the eigen-things of the covariance matrix
 eigen_vals_non_sorted, eigen_vecs_non_sorted = np.linalg.eig(cov_mat)
 
+# 03012021
+
+# e_vec_1 = eigen_vecs_non_sorted[:,0]
+# e_vec_2 = eigen_vecs_non_sorted[:,1]
+
+# print('stop')
+
 idx = np.flip(np.argsort(eigen_vals_non_sorted))
 eigen_vals = eigen_vals_non_sorted[idx]
-eigen_vecs = eigen_vecs_non_sorted[idx]
-
+eigen_vecs = eigen_vecs_non_sorted[:,idx]
 print('\nPC eigenvalues: \n', eigen_vals)
 print('\nPC eigenvectors: \n', eigen_vecs)
 
 # Plot PCs at the plot of the original data
 ax = original_data.axes[0]
 ax.set_aspect('equal', 'box')
-ax.arrow(0, 0, *eigen_vecs[0],
-         head_width=0.2, head_length=0.3, width=0.05, color='red')  # PC1
-ax.arrow(0, 0, *eigen_vecs[1],
-         head_width=0.2, head_length=0.3, width=0.05, color='blue')  # PC2
+ax.arrow(0, 0, *eigen_vecs[:,0],
+         head_width=0.2, head_length=0.3, width=0.05, color='red', label='PC1')  # PC1
+ax.arrow(0, 0, *eigen_vecs[:,1],
+         head_width=0.2, head_length=0.3, width=0.05, color='blue', label='PC2')  # PC2
 # plt.grid()
+plt.legend()
 # plt.tight_layout()
 plt.savefig(os.path.join(OUT_PATH, 'PCA_2D.png'), dpi=600)
 plt.close()
 
 # check if orthogonal:
-PC1 = eigen_vecs[0]
-PC2 = eigen_vecs[1]
+PC1 = eigen_vecs[:,0]
+PC2 = eigen_vecs[:,1]
 res = np.dot(PC1, PC2)
 print(f'PC1 (vector): {PC1}')
 print(f'PC2 (vector): {PC2}')
@@ -123,6 +132,24 @@ product1 = np.matmul(cov_mat, PC1)  # sorting fails dramatically
 product2 = eigen_vals[0] * PC1
 
 print(product1, product2)
+
+'''
+here I plot reconstructed things using only the first PC
+'''
+fig2_ = plt.figure()
+
+WL_transpose = eigen_vecs[:, 0]
+WL_normal = eigen_vecs[:, 0][np.newaxis]  # restricted eigenvectors
+XY_reconstructed = np.matmul(np.transpose(XY), np.outer(WL_transpose, WL_normal))  # X * WL^T * WL
+plt.xlim(-2, 2)
+plt.ylim(-2, 2)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+ax.set_aspect('equal', 'box')
+plt.grid()
+plt.scatter(XY_reconstructed[:, 0], XY_reconstructed[:,1])
+plt.savefig('output/XY_reconstructed_1stPC.png', dpi=600)
+plt.close(fig2_)
 
 ##################### TMP PART #########################################################################################
 ######################## REMOVE IT #####################################################################################
